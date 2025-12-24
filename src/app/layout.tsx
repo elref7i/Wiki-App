fd04a1bfef28a0f2e0e808dd237ab186d31e57d6
Ahmed Refai <momo3956248@gmail.com>
2025-12-24 16:17:49 +0200

    feat: Set up initial Next.js project structure with core UI components and API types.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { NavBar } from "@/components/layouts/nav-bar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        {/* Navbar  */}
+        <NavBar />
+
+        {/* Content */}
+        <main className="p-5">{children}</main>
+      </body>
     </html>
   );
 }
